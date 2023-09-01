932a5f21ba99b8fa3cf3847edc91af8564bb0b13
Karim Gamaleldin <karimgamaleldin74@gmail.com>
2023-09-01 16:39:52 +0300

    SVM kernels

---
+++ SupportVectorMachine/KernelsFormScratch.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy import linalg
+import cvxopt
+import cvxopt.solvers
+
+
+def linear_kernel(x1, x2):
+    return np.dot(x1, x2)
+
+
+def polynomial_kernel(x, y, p=3):
+    return (1 + np.dot(x, y)) ** p
+
+
+def RBF(x, y, sigma=5.0):
+    return np.exp(-linalg.norm(x - y) ** 2 / (2 * (sigma ** 2)))
+
+# todo: create a svm with kernels
--- SupportVectorMachine/SVMFromScratch.py
+++ SupportVectorMachine/SVMFromScratch.py
@@ -76,34 +76,35 @@ class Support_Vector_Machine:
             self.b = opt_choice[1]
             latest_optimum = opt_choice[0][0] + step * 2
 
-    def predict(self,features):
+    def predict(self, features):
         # sign( x.w+b )
-        classification = np.sign(np.dot(np.array(features),self.w)+self.b)
-        if classification !=0 and self.visualization:
+        classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+        print(classification)
+        if classification != 0 and self.visualization:
             self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
         return classification
 
     def visualize(self):
-        [[self.ax.scatter(x[0],x[1],s=100,color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
 
-        def hyperplane(x,w,b,v):
-            return (-w[0]*x-b+v) / w[1]
+        def hyperplane(x, w, b, v):
+            return (-w[0] * x - b + v) / w[1]
 
-        datarange = (self.min_feature_value*0.9,self.max_feature_value*1.1)
+        datarange = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
         hyp_x_min = datarange[0]
         hyp_x_max = datarange[1]
 
         psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
         psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
-        self.ax.plot([hyp_x_min,hyp_x_max],[psv1,psv2], 'k')
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k')
 
         nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
         nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
-        self.ax.plot([hyp_x_min,hyp_x_max],[nsv1,nsv2], 'k')
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], 'k')
 
         db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
         db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
-        self.ax.plot([hyp_x_min,hyp_x_max],[db1,db2], 'y--')
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
 
         plt.show()
 
@@ -111,3 +112,14 @@ class Support_Vector_Machine:
 data_dict = {-1: np.array([[1, 7], [2, 8], [3, 8]]),
              1: np.array([[5, 1], [6, -1], [7, 3]])
              }
+
+clf = Support_Vector_Machine()
+clf.fit(data=data_dict)
+clf.visualize()
+
+predict = [[0, 10],
+           [1, 3],
+           [6, -5]]
+
+for p in predict:
+    clf.predict(p)
